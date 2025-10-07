38f59e5eb3768b7e92706291b24cd622befbed54
NAddink <noahaddink@gmail.com>
2025-10-07 09:58:42 -0700

    Set up activity2

--- Activity2/music/src/index.js
+++ Activity2/music/src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom/client'; // Notice the /client import
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => {
+  return (
+    <div>This is the app!</div>
+  );
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// assignment info was for older version of react
+// this is using newer version
+// https://react.dev/reference/react-dom/client/createRoot
+
+const domNode = document.getElementById('root');
+const root = ReactDOM.createRoot(domNode);
+
+// 3. Render your app using the root object
+root.render(<App />);
