fd338d31b6a50afa429a6b5db4a7e26b5cd26e06
Noah Addink <noahaddink@gmail.com>
2025-10-25 17:05:30 -0700

    Finish routing demo, start part 3

---
+++ Activity3/router/src/AboutUs.js
@@ -0,0 +1,12 @@
+import react from "react";
+
+const AboutThisSite = () => {
+    return (
+        <div>
+            <h1>About our company</h1>
+            <p>We're super cool and have lots of great products here.</p>
+        </div>
+    )
+}
+
+export default AboutThisSite;
---
+++ Activity3/router/src/ContactUs.js
@@ -0,0 +1,14 @@
+import react from "react";
+
+const ContactUs = () => {
+    return (
+        <div>
+            <h2>Noah's Addink-ompany HQ</h2>
+            <p>123 Addink Place</p>
+            <p>Lincoln, Nebraska</p>
+            <p>(123)-456-7890</p>
+        </div>
+    )
+}
+
+export default ContactUs;
